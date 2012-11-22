1cd6116db7fa0d5f4561ce58ec8909a9c9389a0b
dhouha.br <dhouha.br@16fedb50-249e-28bd-be03-af7ee19129a6>
2012-11-22 14:16:08 +0000

    correction de base.sql

---
+++ src/donnees.sql
@@ -0,0 +1,16 @@
+-- ============================================================
+--    suppression des donnees
+-- ============================================================
+
+delete from RESERVATION ;
+delete from SEANCE ;
+delete from PLACE ;
+delete from TARIF ;
+delete from CATEGORIE_SEANCE ;
+delete from CATEGORIE_PLACE ;
+delete from ROLE ;
+delete from ACTEUR ;
+delete from FILM ;
+delete from REALISATEUR ;
+
+commit ;
